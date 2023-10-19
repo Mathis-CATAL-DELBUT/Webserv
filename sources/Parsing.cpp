b03150e57736dea525a214b91067f45d8dd6543f
catal mathis <42ecolepro@gmail.com>
2023-10-19 15:00:03 +0200

    check link

--- sources/Parsing.cpp
+++ sources/Parsing.cpp
@@ -2,248 +2,271 @@
 
 Parsing::Parsing(std::string file)
 {
-    std::ifstream fileStream(file.c_str());
-    if (!fileStream.is_open())
-    {
-        std::cout << "Error: " << file  << ": not foud or no perm" << std::endl;
+	std::ifstream fileStream(file.c_str());
+	if (!fileStream.is_open())
+	{
+		std::cout << "Error: " << file << ": not foud or no perm" << std::endl;
 		std::exit(1);
-    }
-    if (setListen(file) || setServerName(file) || 
-        setRoot(file) || setIndex(file) || 
-        setErrorPage(file) || setWelcome(file) ||
-        setClientMaxBodySize(file) || 
-        setDirectoryListing(file) || setMethod(file) ||
-        setCgiExtension(file) || this->listen.size() == 0)
-    {
-        std::cout << "Error Parsing: " << file << std::endl;
+	}
+	if (setListen(file) || setServerName(file) ||
+		setRoot(file) || setIndex(file) ||
+		setErrorPage(file) || setWelcome(file) ||
+		setClientMaxBodySize(file) ||
+		setDirectoryListing(file) || setMethod(file) ||
+		setCgiExtension(file) || this->listen.size() == 0)
+	{
+		std::cout << "Error Parsing: " << file << std::endl;
 		fileStream.close();
-    	std::exit(1);
-    }
+		std::exit(1);
+	}
 	setErrorName();
 	setTimeout(false);
 	setExtension();
 	setDefaultErrorPage();
-    fileStream.close();
+	fileStream.close();
 }
 
-Parsing::~Parsing(){}
+Parsing::~Parsing()
+{
+}
 
 Parsing::Parsing(const Parsing &copy)
-{*this = copy;}
+{
+	*this = copy;
+}
 
 Parsing &Parsing::operator=(const Parsing &copy)
 {
-    if (this != &copy)
-    {
-        this->listen = copy.listen;
-        this->server_name = copy.server_name;
-        this->root = copy.root;
-        this->index = copy.index;
-        this->error_page = copy.error_page;
-    }
-    return (*this);
+	if (this != &copy)
+	{
+		this->listen = copy.listen;
+		this->server_name = copy.server_name;
+		this->root = copy.root;
+		this->index = copy.index;
+		this->error_page = copy.error_page;
+	}
+	return (*this);
 }
 
 std::vector<int> Parsing::getListen() const
-{return (this->listen);}
+{
+	return (this->listen);
+}
 
 std::string Parsing::getServerName() const
-{return (this->server_name);}
+{
+	return (this->server_name);
+}
 
 std::string Parsing::getRoot() const
-{return (this->root);}
+{
+	return (this->root);
+}
 
 std::string Parsing::getIndex() const
-{return (this->index);}
+{
+	return (this->index);
+}
 
 std::string Parsing::getErrorPage()
-{return (this->error_page);}
+{
+	return (this->error_page);
+}
 
 std::string Parsing::getWelcome() const
-{return (this->welcome);}
+{
+	return (this->welcome);
+}
 
 int Parsing::getClientMaxBodySize() const
-{return (this->client_max_body_size);}
+{
+	return (this->client_max_body_size);
+}
 
 std::string Parsing::getDirectoryListing() const
-{return (this->directory_listing);}
+{
+	return (this->directory_listing);
+}
 
-const std::string&	Parsing::getDefaultErrorPage(const std::string& error_code) 
-{	
+const std::string &Parsing::getDefaultErrorPage(const std::string &error_code)
+{
 	return (default_error[error_code]);
 }
 
-const std::string&	Parsing::getExtension(const std::string& extension) 
+const std::string &Parsing::getExtension(const std::string &extension)
 {
 	return (file_extension[extension]);
 }
 
-bool    Parsing::getMethod(std::string method) const
+bool Parsing::getMethod(std::string method) const
 {
-    for (std::vector<std::string>::size_type i = 0; i < this->method.size(); i++)
-    {
-        if (this->method[i] == method)
-            return (true);
-    }
-    return (false);
+	for (std::vector<std::string>::size_type i = 0; i < this->method.size(); i++)
+	{
+		if (this->method[i] == method)
+			return (true);
+	}
+	return (false);
 }
 
-bool    Parsing::getCgiExtension(std::string extension) const
+bool Parsing::getCgiExtension(std::string extension) const
 {
-    for (std::vector<std::string>::size_type i = 0; i < this->cgi_extension.size(); i++)
-    {
-        if (this->cgi_extension[i] == extension)
-            return (true);
-    }
-    return (false);
+	for (std::vector<std::string>::size_type i = 0; i < this->cgi_extension.size(); i++)
+	{
+		if (this->cgi_extension[i] == extension)
+			return (true);
+	}
+	return (false);
 }
 
-bool	Parsing::getTimeout() const
+bool Parsing::getTimeout() const
 {
 	return (_timeout);
 }
 
-void	Parsing::setTimeout(bool value) {
+void Parsing::setTimeout(bool value)
+{
 	_timeout = value;
 }
 
 int Parsing::setServerName(std::string file)
 {
-    this->server_name = parseSoloElt(file, "server_name");
-    if (this->server_name == "" || checkLink(this->server_name) == 1)
-        return (1);
-    return (0);
+	this->server_name = parseSoloElt(file, "server_name");
+	if (this->server_name == "" || checkLink(this->server_name) == 1)
+		return (1);
+	return (0);
 }
 
 int Parsing::setRoot(std::string file)
 {
-    this->root = parseSoloElt(file, "root");
-    if (this->root == "" || checkLink(this->root) == 1)
-        return (1);
-    return (0);
+	this->root = parseSoloElt(file, "root");
+	if (this->root == "" || checkLink(this->root) == 1)
+		return (1);
+	return (0);
 }
 int Parsing::setIndex(std::string file)
 {
-    this->index = parseSoloElt(file, "index");
-    if (this->index == "" || checkLink(this->index) == 1)
-        return (1);
-    return (0);
+	this->index = parseSoloElt(file, "index");
+	if (this->index == "" || checkLink(this->index) == 1)
+		return (1);
+	return (0);
 }
 int Parsing::setErrorPage(std::string file)
 {
-    this->error_page = parseSoloElt(file, "error_page");
-    if (this->error_page.size() == 0)
-        return (1);
+	this->error_page = parseSoloElt(file, "error_page");
+	if (this->error_page.size() == 0)
+		return (1);
 	if (checkLink(this->error_page) == 1)
 		return (1);
-    return (0);
+	return (0);
 }
-
 
 int Parsing::setWelcome(std::string file)
 {
-    this->welcome = parseSoloElt(file, "welcome_page");
-    if (this->welcome.size() == 0)
-        return (1);
+	this->welcome = parseSoloElt(file, "welcome_page");
+	if (this->welcome.size() == 0)
+		return (1);
 	if (checkLink(this->welcome) == 1)
 		return (1);
-    return (0);
+	return (0);
 }
-
 
 int Parsing::doubleListen(std::vector<int> listen)
 {
-    for (std::vector<int>::size_type i = 0; i < listen.size(); i++)
-    {
-        for (std::vector<int>::size_type j = 0; j < listen.size(); j++)
-        {
-            if (i != j && listen[i] == listen[j])
-                return (1);
-        }
-    }
-    return (0);
+	for (std::vector<int>::size_type i = 0; i < listen.size(); i++)
+	{
+		for (std::vector<int>::size_type j = 0; j < listen.size(); j++)
+		{
+			if (i != j && listen[i] == listen[j])
+				return (1);
+		}
+	}
+	return (0);
 }
 
 int Parsing::setListen(std::string file)
 {
-    std::string line;
-    std::ifstream fd(file.c_str());
-    while (getline(fd, line))
-    {
-        if (removeSpace(line) == "listen")
-        {
-            getline(fd, line);
-            if (removeSpace(line) == "{")
-            {
-                while (getline(fd, line))
-                {
-                    if (removeSpace(line) == "}")
-                    {
-                        if (doubleListen(this->listen) == 1)
-                            return (1);
-                        return (0);
-                    }
-                    else
-                    {
-                        removeSpace(line, file);
-                        if (onlyNumber(line) == 0)
-                            this->listen.push_back(atoi(line.c_str()));
-                    }
-                }
-                return (1);
-            }
-            return (1);
-        }
-    }
-    fd.close();
-    std::cout << "Error: " << file  << ": not foud or no perm" << std::endl;
-    return (1);
+	std::string line;
+	std::ifstream fd(file.c_str());
+	while (getline(fd, line))
+	{
+		if (removeSpace(line) == "listen")
+		{
+			getline(fd, line);
+			if (removeSpace(line) == "{")
+			{
+				while (getline(fd, line))
+				{
+					if (removeSpace(line) == "}")
+					{
+						if (doubleListen(this->listen) == 1)
+							return (1);
+						return (0);
+					}
+					else
+					{
+						removeSpace(line, file);
+						if (onlyNumber(line) == 0)
+							this->listen.push_back(atoi(line.c_str()));
+					}
+				}
+				return (1);
+			}
+			return (1);
+		}
+	}
+	fd.close();
+	std::cout << "Error: " << file << ": not foud or no perm" << std::endl;
+	return (1);
 }
-
 
 int Parsing::setClientMaxBodySize(std::string file)
 {
-    std::string client;
-    client = parseSoloElt(file, "client_max_body_size");
-    if (client == "" || onlyNumber(client) == 1)
-        return (1);
-    this->client_max_body_size = atoi(client.c_str());
-    if (this->client_max_body_size > 10000)
-        return (std::cout << "client_max_body_size is too large" << std::endl, 1);
-    return (0);
+	std::string client;
+	client = parseSoloElt(file, "client_max_body_size");
+	if (client == "" || onlyNumber(client) == 1)
+		return (1);
+	this->client_max_body_size = atoi(client.c_str());
+	if (this->client_max_body_size > 10000)
+		return (std::cout << "client_max_body_size is too large" << std::endl,
+			1);
+	return (0);
 }
 
 int Parsing::setDirectoryListing(std::string file)
 {
-    this->directory_listing = parseSoloElt(file, "directory_listing");
-    if (this->directory_listing.size() == 0 || 
-        (this->directory_listing != "on" && this->directory_listing != "off"))
-        return (1);
-    return (0);
+	this->directory_listing = parseSoloElt(file, "directory_listing");
+	if (this->directory_listing.size() == 0 ||
+		(this->directory_listing != "on" && this->directory_listing != "off"))
+		return (1);
+	return (0);
 }
 
 int Parsing::setMethod(std::string file)
 {
-    this->method = parseMultiEltString(file, "method");
-    if (this->method.size() == 0)
-        return (1);
-    for (std::vector<std::string>::size_type i = 0; i < this->method.size(); i++)
-    {
-        if (this->method[i] != "GET" && this->method[i] != "POST" && this->method[i] != "DELETE" && this->method[i] != "PUT" && this->method[i] != "HEAD" && this->method[i] != "CONNECT" && this->method[i] != "OPTIONS" && this->method[i] != "TRACE" && this->method[i] != "PATCH")
-            return (1);
-    }
-    return (0);
+	this->method = parseMultiEltString(file, "method");
+	if (this->method.size() == 0)
+		return (1);
+	for (std::vector<std::string>::size_type i = 0; i < this->method.size(); i++)
+	{
+		if (this->method[i] != "GET" && this->method[i] != "POST"
+			&& this->method[i] != "DELETE" && this->method[i] != "PUT"
+			&& this->method[i] != "HEAD" && this->method[i] != "CONNECT"
+			&& this->method[i] != "OPTIONS" && this->method[i] != "TRACE"
+			&& this->method[i] != "PATCH")
+			return (1);
+	}
+	return (0);
 }
 
 int Parsing::setCgiExtension(std::string file)
 {
-    this->cgi_extension = parseMultiEltString(file, "cgi_extension");
-    if (this->cgi_extension.size() == 0)
-        return (1);
-    return (0);
+	this->cgi_extension = parseMultiEltString(file, "cgi_extension");
+	if (this->cgi_extension.size() == 0)
+		return (1);
+	return (0);
 }
 
-void	Parsing::setDefaultErrorPage()
+void Parsing::setDefaultErrorPage()
 {
 	default_error["400"] = "<html><head><title>400 Bad Request</title></head><body><center><h1>400 Bad Request</h1></center><hr><center>Webserv</center></body></html>";
 	default_error["403"] = "<html><head><title>403 Forbidden</title></head><body><center><h1>403 Forbidden</h1></center><hr><center>Webserv</center></body></html>";
@@ -256,11 +279,12 @@ void	Parsing::setDefaultErrorPage()
 	default_error["505"] = "<html><head><title>505 HTTP Version Not Supported</title></head><body><center><h1>505 HTTP Version Not Supported</h1></center><hr><center>Webserv</center></body></html>";
 }
 
-void	Parsing::setExtension() {
+void Parsing::setExtension()
+{
 	file_extension["jpg"] = "image/jpeg";
 	file_extension["html"] = "text/html";
-    file_extension["css"] = "text/css";
-    file_extension["js"] = "text/javascript";
+	file_extension["css"] = "text/css";
+	file_extension["js"] = "text/javascript";
 	file_extension["webp"] = "image/webp";
 	file_extension["png"] = "image/png";
 	file_extension["ico"] = "image/x-icon";
@@ -281,7 +305,7 @@ void	Parsing::setExtension() {
 	file_extension["xml"] = "application/xml";
 }
 
-void			Parsing::setErrorName()
+void Parsing::setErrorName()
 {
 	_errorname[100] = "Continue";
 	_errorname[200] = "OK";
@@ -298,121 +322,128 @@ void			Parsing::setErrorName()
 	_errorname[500] = "Internal Server Error";
 }
 
-const std::string&	Parsing::getErrorName(int status) {
+const std::string &Parsing::getErrorName(int status)
+{
 	return (_errorname[status]);
 }
 
-
 void Parsing::removeSpace(std::string &str, std::string file)
 {
-    int i = 0;
-    while (str[i] == ' ' || str[i] == '\t')
-        str.erase(i, 1);
-    while (str[i])
-            i++;
-    if (str[i - 1] != ';')
-    {
-        std::cout << "Error Parsing: " << file << std::endl;
-        std::exit(1);
-    }
-    str.erase(i - 1, 1);
-}
+	int	i;
 
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t')
+		str.erase(i, 1);
+	while (str[i])
+		i++;
+	if (str[i - 1] != ';')
+	{
+		std::cout << "Error Parsing: " << file << std::endl;
+		std::exit(1);
+	}
+	str.erase(i - 1, 1);
+}
 
 std::string Parsing::removeSpace(std::string &str)
 {
-    int i = 0;
-    std::string result = "";
-    while (str[i])
-    {
-        if (str[i] != ' ')
-            result += str[i];
-        i++;
-    }
-    return (result);
+	int	i;
+
+	i = 0;
+	std::string result = "";
+	while (str[i])
+	{
+		if (str[i] != ' ')
+			result += str[i];
+		i++;
+	}
+	return (result);
 }
 
 int Parsing::onlyNumber(std::string str)
 {
-    int i = 0;
-    while (str[i])
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return (1);
-        i++;
-    }
-    return (0);
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 int Parsing::checkLink(std::string str)
 {
-    int i = 0;
-    while (str[i] )
-    {
-        if (str[i] == ' ')
-            return (1);
-        i++;
-    }
-    return (0);
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 std::string Parsing::parseSoloElt(std::string file, std::string name)
 {
-    std::string result;
-    std::string line;
-    std::ifstream fd(file.c_str());
-    while (getline(fd, line))
-    {
-        if (removeSpace(line) == name)
-        {
-            getline(fd, line);
-            if (removeSpace(line) == "{")
-            {
-                getline(fd, line);
-                result = line;
-                getline(fd, line);
-                if (removeSpace(line) == "}")
-                    return (removeSpace(result, file), result);
-                return ("");
-            }
-            return ("");
-        }
-    }
-    return ("");
+	std::string result;
+	std::string line;
+	std::ifstream fd(file.c_str());
+	while (getline(fd, line))
+	{
+		if (removeSpace(line) == name)
+		{
+			getline(fd, line);
+			if (removeSpace(line) == "{")
+			{
+				getline(fd, line);
+				result = line;
+				getline(fd, line);
+				if (removeSpace(line) == "}")
+					return (removeSpace(result, file), result);
+				return ("");
+			}
+			return ("");
+		}
+	}
+	return ("");
 }
 
-std::vector<std::string> Parsing::parseMultiEltString(std::string file, std::string name)
+std::vector<std::string> Parsing::parseMultiEltString(std::string file,
+		std::string name)
 {
-    std::vector<std::string> result;
-    std::string line;
-    std::ifstream fd(file.c_str());
-    while (getline(fd, line))
-    {
-        if (removeSpace(line) == name)
-        {
-            getline(fd, line);
-            if (removeSpace(line) == "{")
-            {
-                while (getline(fd, line))
-                {
-                    if (removeSpace(line) == "}")
-                        return (result);
-                    else
-                    {
-                        if (name == "listen")
-                            result.push_back(line.c_str());
-                        else
-                        {
-                            removeSpace(line, file);
-                            result.push_back(line);
-                        }
-                    }
-                }
-                return std::vector<std::string>();
-            }
-            return std::vector<std::string>();
-        }
-    }
-    return std::vector<std::string>();
+	std::vector<std::string> result;
+	std::string line;
+	std::ifstream fd(file.c_str());
+	while (getline(fd, line))
+	{
+		if (removeSpace(line) == name)
+		{
+			getline(fd, line);
+			if (removeSpace(line) == "{")
+			{
+				while (getline(fd, line))
+				{
+					if (removeSpace(line) == "}")
+						return (result);
+					else
+					{
+						if (name == "listen")
+							result.push_back(line.c_str());
+						else
+						{
+							removeSpace(line, file);
+							result.push_back(line);
+						}
+					}
+				}
+				return (std::vector<std::string>());
+			}
+			return (std::vector<std::string>());
+		}
+	}
+	return (std::vector<std::string>());
 }
-
